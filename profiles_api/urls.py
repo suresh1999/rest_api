1dd28f445cbf7a500bb2bc153c7fa2308d260de1
Suresh Kumar <mail.sk1999@gmail.com>
2019-02-04 23:34:04 +0530

    completed profiles feed

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,6 +8,7 @@ router = DefaultRouter()
 router.register('hello-viewset', HelloViewSet, base_name='hello-viewset')
 router.register('profile', UserProfileViewSet)
 router.register('login', LoginViewSet, base_name="login-viewset")
+router.register('feed', UserProfileFeedViewSet)
 
 urlpatterns = [
 	path('hello-view/', HelloApiView.as_view()),
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -10,7 +10,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 # Create your views here.
 
 class HelloApiView(APIView):
@@ -107,4 +107,18 @@ class LoginViewSet(viewsets.ViewSet):
 	def create(self, request):
 		return ObtainAuthToken().post(request)
 
-		
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+
+	authentication_classes = (TokenAuthentication, )
+
+	serializer_class = serializers.ProfileFeedItemSerializer
+
+	queryset = models.ProfileFeedItem.objects.all()
+
+	permission_classes = ( permissions.PostOwnStatus, IsAuthenticatedOrReadOnly,)
+
+	def perform_create(self, serializer):
+
+		serializer.save(user_profile=self.request.user)
+
+
