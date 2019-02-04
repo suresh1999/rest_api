c4d363da76a36049f6776cbae73b2b33a490f910
Suresh Kumar <mail.sk1999@gmail.com>
2019-02-04 22:14:15 +0530

    added searc filters

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from . import models
 from . import permissions
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 # Create your views here.
 
 class HelloApiView(APIView):
@@ -93,3 +94,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 	queryset = models.UserProfile.objects.all()
 	authentication_classes = (TokenAuthentication, )
 	permission_classes = (permissions.UpdateOwnProfile, )
+	filter_backends = (filters.SearchFilter,)
+	search_fields = ('name', 'email')
